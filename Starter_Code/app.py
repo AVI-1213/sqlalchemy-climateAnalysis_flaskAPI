9871cb2a76b479e5cf4ef79f79b88b60bf79355a
AVI-1213 <avinashsingh1213@icloud.com>
2024-05-08 01:12:09 -0400

    app.py updated

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,11 +1,11 @@
 # Import the dependencies.
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, text, func
 from sqlalchemy.orm import Session
 
 import datetime as dt
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 import numpy as np
 
 
@@ -23,8 +23,20 @@ Base.prepare(autoload_with=engine)
 measurement = Base.classes.measurement
 station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine) 
+# defining a function to find most recent dates
+def previous_year():
+# creating a session
+    session = Session(engine)
+    
+    recent_date = session.query(measurement.date).order_by(measurement.date.desc()).first()
+    first_year_date = dt.date(2017,8,23)-dt.timedelta(days=365)
+    
+    session.close()
+
+    return (first_year_date)
+
+ # Create our session (link) from Python to the DB
+#session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -41,40 +53,76 @@ app = Flask(__name__)
 #   List all the available routes.
 
 @app.route("/")
-def welcome():
+def homepage():
     return (
-        f"Welcome to Climate Analysis of Hawaii API" <br>
-        f"List of available routes"<br>
-        f"/api/v1.0/precipitation" <br>
-        f"/api/v1.0/stations"<br>
-        f"/api/v1.0/tobs"<br>
-        f"/api/v1.0/start (as YYYY-MM-DD)"<br>
-        f"/api/v1.0/start/end(as YYYY-MM-DD)"<br>
+        f"Welcome to Climate Analysis of Hawaii API" 
+        f"List of available routes"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/start (as YYYY-MM-DD)"
+        f"/api/v1.0/start/end(as YYYY-MM-DD)"
         )
 
 
 #2
 #     /api/v1.0/precipitation
 #     Convert the query results from your precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
-#     Return the JSON representation of your dictionary.
 
+@app.route("/api/v1.0/precipitation")
+def percipitation():
+# Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    annual_percp = session.query(measurement.date, measurement.prcp).filter(measurement.date>= previous_year()).all()
+    session.close()
+# Return the JSON representation of your dictionary.
+    prcp_date = []
+    prcp_total = []
+    for date,prcp in annual_percp:
+        prcp_date.append(date)
+        prcp_total.append(prcp)
+    prcp_dict = dict(zip(prcp_date,prcp_total))
+    
+    return jsonify(prcp_dict)
 
 #3
 #   /api/v1.0/stations
 #   Return a JSON list of stations from the dataset.
+@app.route("/api/v1.0/stations")
+def station():
+    session = Session(engine)
+    station_results = session.query(station.station).all()
+    
+    session.close()
+# Convert list of tuples into normal list
+    station_list = list(np.ravel(station_results))
 
-
-
+    return jsonify(station_list)
 
 
 #4
 #   /api/v1.0/tobs
 #   Query the dates and temperature observations of the most-active station for the previous year of data.
 #   Return a JSON list of temperature observations for the previous year.
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    
+    temp_obs = session.query(measurement.tobs).filter(measurement.station == 'USC00519281').\
+                filter(measurement.date>= previous_year()).all()
+    
+    session.close()
 
+    tobs_list = []
+    for date,tobs in temp_obs:
+        temp_dict = {}
+        temp_dict["date"] = date
+        temp_dict["tobs"] = tobs
+        tobs_list.append(temp_dict)
 
-
-
+        return jsonify(temp_dict)
+    
 #5
 #   /api/v1.0/<start> and /api/v1.0/<start>/<end>
 #     Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range.
@@ -84,3 +132,6 @@ def welcome():
 
 
 
+# Define main branch 
+if __name__ == "__main__":
+    app.run(debug = True)
